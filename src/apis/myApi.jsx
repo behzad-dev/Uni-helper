2e5e795dc611e68ad138a7d0056b91011cfa0c19
behzad-dev <70510985+behzad-dev@users.noreply.github.com>
2020-11-03 21:52:47 +0330

    API explanation

--- src/apis/myApi.jsx
+++ src/apis/myApi.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
 export const myApi = axios.create({
+  //Here You Should Set Your Server, this Application using GraphQL for endpoint.
   baseURL: "http://localhost:6233/graphql",
 });
