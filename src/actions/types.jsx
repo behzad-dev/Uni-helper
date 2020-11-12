8f2f8fc50e26d4b28e921b9d1bbe86642a9372c6
behzad-dev <70510985+behzad-dev@users.noreply.github.com>
2020-11-12 21:15:29 +0330

    Update types.jsx

--- src/actions/types.jsx
+++ src/actions/types.jsx
@@ -1,3 +1,4 @@
+//Different kind of variabales defined here to be easier to use in other places
 export const RETRIEVE_GRAPHQL = "RETRIEVE_GRAPHQL";
 export const LOGIN_GRAPHQL = "LOGIN_GRAPHQL";
 export const CREATEPOST_GRAPHQL = "CREATEPOST_GRAPHQL";
