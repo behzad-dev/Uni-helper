a3282ea821543ed03e4dfebe7dbccb17fd51a83a
behzad-dev <70510985+behzad-dev@users.noreply.github.com>
2020-11-20 22:36:38 +0330

    Update myApi.jsx

--- src/apis/myApi.jsx
+++ src/apis/myApi.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 export const myApi = axios.create({
   //Here You Should Set Your Server, this Application using GraphQL for endpoint.
+  //In order to Access data from anywhere, you should get a public server.
+  // or if it is only for your usage, you can use localhost
   baseURL: "http://localhost:6233/graphql",
 });
